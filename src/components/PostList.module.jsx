26b4380a19dae8b849812e468485f1db4006c9f0
Jeet Kaushik <78296225+Iamjeetkaushik@users.noreply.github.com>
2023-06-15 23:54:36 +0530

    Modal and state changes

--- src/components/PostList.module.jsx
+++ src/components/PostList.module.jsx
@@ -1,16 +1,31 @@
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './Postlist.css';
+import { useState } from 'react';
+import Modal from './Modal';
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
-      <ul className={classes.posts}>
-        <Post
-          author='Jeet Kaushik'
-          body="He knew he wasn't a 10, but you don't have to make him feel like 0."
+      <Modal>
+        <NewPost
+          onBodyChange={changeBodyHandler}
+          onAuthorChange={changeAuthorHandler}
         />
+      </Modal>
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post
           author='Jeet Kaushik'
           body="He knew he wasn't a 10, but you don't have to make him feel like 0."
