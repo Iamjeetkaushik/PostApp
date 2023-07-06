7059bf9f1709115eb67aabc42383e905111b3482
Jeet Kaushik <78296225+Iamjeetkaushik@users.noreply.github.com>
2023-07-06 18:00:02 +0530

    navigation and more hooks used

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Posts, { loader as postLoader } from '../routes/Posts';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NewPost, { action as postActions } from '../routes/NewPost';
+import RootLayout from '../routes/RootLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Posts />,
+        children: [{ path: '/Modal', element: <NewPost /> }],
+        loader: postLoader,
+        action: postActions,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
